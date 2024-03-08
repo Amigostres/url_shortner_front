e4d5ee18305c31d369b0e947f4eb6c60e7082356
Jose <josemanuel21302@gmail.com>
2024-03-07 22:46:22 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [shortUrl, setshortUrl] = useState('url_test.com')
+
+  return (
+    <>
+      <form>
+        <label >
+          Url:
+          <input type="text" name="Url" />
+        </label>
+        
+        <input type="submit" value="Submit" />
+      </form>
+      <p>
+        your new url: 
+        <a href='#'>{shortUrl}</a>
+      </p>
+    </>
+  )
+}
+
+export default App
